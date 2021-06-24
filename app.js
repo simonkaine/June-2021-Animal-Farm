d2a207f7bb2e1ba48888f992d33d6fac1f50666c
Simon Kaine <simonkaine@gmail.com>
2021-06-23 17:44:07 -0700

    Added animal noises and images

--- app.js
+++ app.js
@@ -7,3 +7,48 @@
   // get user input(s)
   // do any needed work with the value(s)
   // update DOM to reflect new value(s)
+
+// 
+
+
+const cat = document.getElementById('cat');
+const catSound = document.getElementById('cat-sound');
+cat.addEventListener('click', ()=> {
+    catSound.play();
+});
+
+const dog = document.getElementById('dog');
+const dogSound = document.getElementById('dog-sound');
+dog.addEventListener('click', ()=> {
+    dogSound.play();
+});
+
+const horse = document.getElementById('horse');
+const horseSound = document.getElementById('horse-sound');
+horse.addEventListener('click', ()=> {
+    horseSound.play();
+});
+
+const catKey = document.getElementById('cat-key');
+catKey.addEventListener('keydown', (event)=> {
+
+  if (event.key === "c" || event.key === "C")
+    catSound.play();
+
+});
+
+const dogKey = document.getElementById('dog-key');
+dogKey.addEventListener('keydown', (event)=> {
+
+  if (event.key === "d" || event.key === "D")
+    dogSound.play();
+
+});
+
+const horseKey = document.getElementById('horse-key');
+horseKey.addEventListener('keydown', (event)=> {
+
+  if (event.key === "h" || event.key === "H")
+    horseSound.play();
+
+});
